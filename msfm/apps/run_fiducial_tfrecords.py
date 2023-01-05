48b48fbc2d52df13e5fda68e60296b1066b50da1
Arne Thomsen <athomsen@phys.ethz.ch>
2023-01-05 15:09:14 +0100

    added fiducial .tfrecords for the delta loss

---
+++ msfm/apps/run_fiducial_tfrecords.py
@@ -0,0 +1,218 @@
+# Copyright (C) 2022 ETH Zurich, Institute for Particle Physics and Astrophysics
+
+"""
+Created December 2022
+Author: Arne Thomsen
+
+Convert the .h5 files containing the lensing and clustering maps to scrambled .tfrecord files suitable for training
+with the delta loss at the fiducial cosmology and its perturbations
+
+Adapted from https://github.com/tomaszkacprzak/CosmoPointNet/blob/main/CosmoPointNet/apps/run_build_tfrecords.py 
+by Tomasz Kacprzak
+and 
+https://cosmo-gitlab.phys.ethz.ch/jafluri/arne_handover/-/blob/main/map_projection/tfr_generation/fiducial.py
+https://cosmo-gitlab.phys.ethz.ch/jafluri/cosmogrid_kids1000/-/blob/master/kids1000_analysis/input_pipeline.py
+https://cosmo-gitlab.phys.ethz.ch/jafluri/cosmogrid_kids1000/-/blob/master/kids1000_analysis/data.py
+by Janis Fluri
+"""
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+import os, argparse, warnings, h5py
+
+from numpy.random import default_rng
+from numba import njit
+from icecream import ic
+
+from msfm.utils import logging, input_output, cosmogrid, tfrecords
+from msfm.utils.filenames import *
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+warnings.filterwarnings("once", category=UserWarning)
+LOGGER = logging.get_logger(__file__)
+
+
+def resources(args):
+    return dict(main_memory=1000, main_time=4, main_scratch=0, main_n_cores=8)
+
+
+def setup(args):
+    description = "Make weak lensing and intrinsic alignment datavectors from full sky maps"
+    parser = argparse.ArgumentParser(description=description, add_help=True)
+
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        type=str,
+        default="info",
+        choices=("critical", "error", "warning", "info", "debug"),
+        help="logging level",
+    )
+    parser.add_argument("--with_bary", action="store_true", help="activate debug mode")
+    parser.add_argument("--n_files", type=int, default=100, help="number of .tfrecord files to produce")
+    parser.add_argument(
+        "--dir_in",
+        type=str,
+        default="/global/cfs/cdirs/des/cosmogrid/DESY3/grid",
+        help="input root dir of the simulations",
+    )
+    parser.add_argument(
+        "--dir_out",
+        type=str,
+        default="/pscratch/sd/a/athomsen/DESY3/grid",
+        help="output root dir of the .tfrecords",
+    )
+    parser.add_argument(
+        "--repo_dir",
+        type=str,
+        default="/global/homes/a/athomsen/multiprobe-simulation-forward-model",
+        help="root dir of the msfm repo to convert relative paths to absolute ones",
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="configs/config.yaml",
+        help="configuration yaml file",
+    )
+    parser.add_argument("--np_seed", type=int, default=7, help="random seed to shuffle the patches")
+    parser.add_argument("--debug", action="store_true", help="activate debug mode")
+
+    args, _ = parser.parse_known_args(args)
+
+    logging.set_all_loggers_level(args.verbosity)
+
+    return args
+
+
+def main(indices, args):
+    args = setup(args)
+
+    LOGGER.timer.start("main")
+
+    conf_file = os.path.join(args.repo_dir, args.config)
+    conf = input_output.read_yaml(conf_file)
+    LOGGER.info(f"Loaded configuration file")
+
+    meta_info_file = os.path.join(args.repo_dir, conf["files"]["meta_info"])
+    params_info = cosmogrid.get_parameter_info(meta_info_file, "fiducial")
+
+    # constants
+    n_patches = conf["analysis"]["n_patches"]
+    n_perms_per_param = conf["analysis"]["fiducial"]["n_perms_per_param"]
+    n_examples_per_param = n_patches * n_perms_per_param
+
+    # set up the paths
+    meta_info_file = os.path.join(args.repo_dir, conf["files"]["meta_info"])
+    params_info = cosmogrid.get_parameter_info(meta_info_file, "fiducial")
+    params_dir = [param_dir.decode("utf-8") for param_dir in params_info["path_par"]]
+
+    # remove baryon perturbations for the fiducial set
+    if not args.with_bary:
+        params_dir = [param_dir for param_dir in params_dir if not "bary" in param_dir]
+
+    params_dir_in = [os.path.join(args.dir_in, param_dir) for param_dir in params_dir]
+
+    n_params = len(params_dir_in)
+    LOGGER.info(f"Got simulation set fiducial of size {n_params} with base path {args.dir_in}")
+
+    n_files = len(indices)
+    if n_examples_per_param % n_files == 0:
+        n_examples_per_file = n_examples_per_param // n_files
+    else:
+        raise ValueError(
+            f"The total number of examples per parameter {n_examples_per_param} "
+            f"has to be divisible by the number of files {n_files}"
+        )
+
+    # shuffle the indices
+    rng = default_rng(seed=args.np_seed)
+    i_examples = rng.permutation(n_examples_per_param)
+
+    LOGGER.debug(f"n_files = {n_files}")
+    LOGGER.debug(f"n_examples_per_file = {n_examples_per_file}")
+
+    # index corresponds to a .tfrecord file ###########################################################################
+    for index in indices:
+        LOGGER.timer.start("index")
+
+        file_tfrecord = get_filename_tfrecords(
+            args.dir_out, tag=conf["survey"]["name"], index=index, simset="fiducial"
+        )
+        LOGGER.info(f"Index {index} is writing to {file_tfrecord}")
+
+        js = index * n_examples_per_file
+        je = (index + 1) * n_examples_per_file
+
+        n_done = 0
+        with tf.io.TFRecordWriter(file_tfrecord) as file_writer:
+
+            for j in LOGGER.progressbar(range(js, je), at_level="info", desc="Storing DES patches"):
+                if args.debug:
+                    if n_done > 5:
+                        LOGGER.warning("Debug mode, aborting after 5 subindices")
+                        break
+
+                # get the indices to the patch
+                i_example = i_examples[j]
+                LOGGER.debug(f"j = {j} in range({js},{je}): i_example={i_example}")
+
+                # maps, loop over the perturbations in the right order
+                kg_perts, ia_perts, sn_perts = [], [], []
+                for param_dir_in in params_dir_in:
+                    file_param = get_filename_data_vectors(param_dir_in, with_bary=args.with_bary)
+                    kg, ia, sn = load_datavectors(file_param, i_example)
+
+                    kg_perts.append(kg)
+                    ia_perts.append(ia)
+                    sn_perts.append(sn)
+
+                # shape (2 * n_params + 1, n_pix, n_z_bins) for the delta loss
+                kg_perts = np.stack(kg_perts, axis=0)
+                ia_perts = np.stack(ia_perts, axis=0)
+                sn_perts = np.stack(sn_perts, axis=0)
+
+                serialized = tfrecords.parse_forward_fiducial(kg_perts, ia_perts, sn_perts).SerializeToString()
+
+                # check correctness
+                inv_kg, inv_ia, inv_sn = tfrecords.parse_inverse_fiducial(serialized)
+                assert np.allclose(inv_kg, kg)
+                assert np.allclose(inv_ia, ia)
+                assert np.allclose(inv_sn, sn)
+
+                file_writer.write(serialized)
+
+                n_done += 1
+
+        LOGGER.info(f"Done with index {index} after {LOGGER.timer.elapsed('index')}")
+        yield index
+
+
+def load_datavectors(filename, ind_example):
+    with h5py.File(filename, "r") as f:
+        # shape (n_examples_per_param, n_pix, n_z_bins)
+        kg = f["kg"][ind_example, ...]
+        ia = f["ia"][ind_example, ...]
+        sn = f["sn"][ind_example, ...]
+
+    LOGGER.debug(f"Successfully loaded file {filename}")
+    return kg, ia, sn
+
+
+# This main only exists for testing purposes when not using esub
+if __name__ == "__main__":
+    args = [
+        "--dir_in=/Users/arne/data/CosmoGrid_example/DES/grid",
+        "--dir_out=/Users/arne/data/CosmoGrid_example/DES/grid",
+        "--repo_dir=/Users/arne/git/multiprobe-simulation-forward-model",
+        "--config=configs/config.yaml",
+        "--debug",
+        "--verbosity=debug",
+    ]
+
+    indices = [0, 1, 2, 3]
+    for _ in main(indices, args):
+        pass
+
+    LOGGER.info(f"Done with main after {LOGGER.timer.elapsed('main')}")
--- msfm/utils/tfrecords.py
+++ msfm/utils/tfrecords.py
@@ -70,6 +70,71 @@ def parse_inverse_maps(element):
     # return kg, ia, sn, dg, cosmo, sobol
     return kg, ia, sn, cosmo, sobol
 
+def parse_forward_fiducial(kg_perts, ia_perts, sn_perts):
+    """ The fiducials don't need a label and contain the perturbation for the delta loss with
+    n_perts = 2 * n_params + 1
+
+    Args:
+        kg_perts (np.ndarray): shape(n_perts, n_pix, n_z_bins)
+        ia_perts (_type_): shape(n_perts, n_pix, n_z_bins)
+        sn_perts (_type_): shape(n_perts, n_pix, n_z_bins)
+
+    Returns:
+        _type_: _description_
+    """
+    # assert kg.shape == ia.shape == sn.shape == dg.shape
+    assert kg_perts.shape == ia_perts.shape == sn_perts.shape
+
+    # define the structure of a single example
+    data = {
+        # tensor shapes
+        "n_perts": _int64_feature(kg_perts.shape[0]),
+        "n_pix": _int64_feature(kg_perts.shape[1]),
+        "n_z_bins": _int64_feature(kg_perts.shape[2]),
+        # lensing, metacal
+        "kg": _bytes_feature(tf.io.serialize_tensor(kg_perts)),
+        "ia": _bytes_feature(tf.io.serialize_tensor(ia_perts)),
+        "sn": _bytes_feature(tf.io.serialize_tensor(sn_perts)),
+        # clustering, maglim TODO
+        # "dg": _bytes_feature(tf.io.serialize_tensor(dg)),
+    }
+
+    # create an Example, wrapping the single features
+    out = tf.train.Example(features=tf.train.Features(feature=data))
+    return out
+
+def parse_inverse_fiducial(element):
+    """use the same structure as above"""
+
+    data = {
+        # tensor shapes
+        "n_perts": tf.io.FixedLenFeature([], tf.int64),
+        "n_pix": tf.io.FixedLenFeature([], tf.int64),
+        "n_z_bins": tf.io.FixedLenFeature([], tf.int64),
+        # lensing, metacal
+        "kg": tf.io.FixedLenFeature([], tf.string),
+        "ia": tf.io.FixedLenFeature([], tf.string),
+        "sn": tf.io.FixedLenFeature([], tf.string),
+        # clustering, maglim TODO
+        # "dg": tf.io.FixedLenFeature([], tf.string),
+    }
+
+    content = tf.io.parse_single_example(element, data)
+
+    kg = tf.io.parse_tensor(content["kg"], out_type=tf.float32)
+    ia = tf.io.parse_tensor(content["ia"], out_type=tf.float32)
+    sn = tf.io.parse_tensor(content["sn"], out_type=tf.float32)
+    # dg = tf.io.parse_tensor(content["dg"], out_type=tf.float32)
+
+    kg = tf.reshape(kg, shape=(content["n_perts"], content["n_pix"], content["n_z_bins"]))
+    ia = tf.reshape(ia, shape=(content["n_perts"], content["n_pix"], content["n_z_bins"]))
+    sn = tf.reshape(sn, shape=(content["n_perts"], content["n_pix"], content["n_z_bins"]))
+    # dg = tf.reshape(dg, shape=(content["n_pix"], content["n_z_bins"]))
+
+    # return kg, ia, sn, dg, cosmo, sobol
+    return kg, ia, sn
+
+# features ############################################################################################################
 
 # https://www.tensorflow.org/tutorials/load_data/tfrecord#data_types_for_tftrainexample
 def _bytes_feature(value):
