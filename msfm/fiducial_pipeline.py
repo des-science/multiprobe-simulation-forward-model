02fe036f168def18df5f8916927872c412142717
Arne Thomsen <athomsen@student.ethz.ch>
2023-03-13 01:36:50 -0700

    combined all fiducial augmentations into one function

--- msfm/fiducial_pipeline.py
+++ msfm/fiducial_pipeline.py
@@ -22,72 +22,74 @@ warnings.filterwarnings("once", category=UserWarning)
 LOGGER = logger.get_logger(__file__)
 
 
-def dset_add_bias(data_vectors, index, pert_labels, m_bias_dist=None):
-    """Adds a random multiplicative shear bias (the additive one is negligible)
+def dset_augmentations(
+    data_vectors: dict,
+    index: tuple,
+    pert_labels: list,
+    m_bias_dist: tfp.distributions.Distribution = None,
+    noise_scale: float = 1.0,
+    masks: tf.tensor = None,
+):
+    """Applies random augmentations and general pre-processing to the kappa maps. This includes in order:
+        - Adds random multiplicative shear bias (the additive one is negligible) to the kappa maps
+        - Adds the chosen shape noise realization to the kappa maps
+        - Masks the resulting data vector (this is only required if an addition like an additive shear bias is applied)
+        - Concatenates the batch and perturbations along axis 0 (for compatibility with the delta loss)
 
     Args:
-        data_vectors (dict): has keys "kg_{pert_label}" and "sn", which contain tensors of shape (n_pix, n_z_bins)
-        index (tf.tensor): Integer that is just passed through
+        data_vectors (dict): Has keys "kg_{pert_label}" and "sn", which contain tensors of shape
+            (batch_size, n_pix, n_z_bins).
+        index (tf.tensor): Tuple of integers (i_example, i_noise) that is only passed through.
         pert_labels (list): The labels of the perturbations to loop through. These are needed explicitly for the
             function to be converted by autograph.
         m_bias_dist (tfp.distributions.Distribution, optional): TensorFlow probability distribution from which the bias
             is sampled. The samples have to be drawn within this function for randomness. Defaults to None, then the
-            bias is equal to one.
+            bias is always equal to one.
+        noise_scale (float): Multiplicative factor applied to the shape noise before it is added. Defaults to 1.0,
+            for a value of None, no shape noise is added to the kappa maps.
+        masks (tf.tensor): Tensor Of shape (n_pix, n_z_bins) only containing zeros and ones.
 
     Returns:
-        tuple: (data_vectors, index) of the same shape as at the input
-    """
-    LOGGER.warning(f"Tracing dset_add_bias")
-
-    if m_bias_dist is None:
-        LOGGER.warning(f"No multiplicative shear bias is applied")
-
-    elif isinstance(m_bias_dist, tfp.distributions.Distribution):
-        m_bias = m_bias_dist.sample()
-        for label in pert_labels:
-            # broadcast axis 0 of size n_pix
-            data_vectors[f"kg_{label}"] *= 1 + m_bias
-
-    return data_vectors, index
-
-
-def dset_add_noise(data_vectors, index, pert_labels, noise_scale=1):
-    """
-    Args:
-        data_vectors (dict): has keys "kg_{pert_label}" and "sn", which contain tensors of shape (n_pix, n_z_bins)
-        index (tf.tensor): Integer that is just passed through
-        pert_labels (list): The labels of the perturbations to loop through. These are needed explicitly for the
-            function to be converted by autograph.
-
-    Returns:
-        tuple: (data_vectors, index), but data_vectors["sn"] is removed
-    """
-    LOGGER.warning(f"Tracing dset_add_noise")
-
-    sn = data_vectors.pop("sn")
-    for label in pert_labels:
-        data_vectors[f"kg_{label}"] += noise_scale * sn
-
-    return data_vectors, index
-
-
-def dset_concat_perts(data_vectors, index, pert_labels):
-    """Concatenates the batch and perturbations along axis 0
-
-    Args:
-        data_vectors (dict): has keys "kg_{pert_label}", which contain tensors of shape (n_pix, n_z_bins)
-        index (tf.tensor): Integer that is just passed through
-        pert_labels (list): The labels of the perturbations to loop through. These are needed explicitly for the
-            function to be converted by autograph.
-
-    Returns:
-        (tf.tensor, tf.tensor): data_vectors is a tensor of shape (n_perts * batch_size, n_pix), where the first
+        tuple: (data_vectors, index), where data_vectors is a tensor of shape (n_perts * batch_size, n_pix). The first
             batch_size elements correspond to the fiducial value, the second to the first perturbation, the third to
-            the second perturbation, etc.
+            the second perturbation, etc. (for compatibility with the delta loss). Furthermore, data_vectors["sn"] is
+            removed.
     """
-    LOGGER.warning(f"Tracing dset_concat_perts")
 
-    data_vectors = tf.concat([data_vectors[f"kg_{pert_label}"] for pert_label in pert_labels], axis=0)
+    LOGGER.warning(f"Tracing dset_augmentations")
+
+    # shape noise
+    sn = data_vectors.pop("sn")
+
+    augmented_dvs = []
+    for label in pert_labels:
+        data_vector = data_vectors[f"kg_{label}"]
+
+        # shear bias
+        if m_bias_dist is not None:
+            # shape (n_z_bins,)
+            m_bias = m_bias_dist.sample()
+            # broadcast axis 0 of size n_pix
+            data_vector *= 1.0 + m_bias
+        else:
+            LOGGER.warning(f"No multiplicative shear bias is applied")
+
+        # shape noise
+        if noise_scale is not None:
+            data_vector += noise_scale * sn
+        else:
+            LOGGER.warning(f"No shape noise is added")
+
+        # masking
+        if masks is not None:
+            data_vector *= masks
+        else:
+            LOGGER.warning(f"No masking is applied")
+
+        augmented_dvs.append(data_vector)
+
+    # concatenate
+    data_vectors = tf.concat(augmented_dvs, axis=0)
 
     return data_vectors, index
 
@@ -102,8 +104,9 @@ def get_fiducial_dset(
     i_noise: int = 0,
     noise_scale: float = 1.0,
     # performance
+    is_cached: bool = False,
     n_readers: int = 8,
-    n_prefetch: int = tf.data.AUTOTUNE,
+    n_prefetch: int = None,
     file_name_shuffle_buffer: int = 128,
     examples_shuffle_buffer: int = 128,
     # random seeds
@@ -119,8 +122,8 @@ def get_fiducial_dset(
 
     Args:
         tfr_pattern (str): Glob pattern of the .fiducial tfrecord files.
-        batch_size (int): Local batch size, will be multiplied with the number of deltas for the total batch size.
-        params (list): List of the cosmological parameters with respect to which the perturbations to be used in 
+        local_batch_size (int): Local batch size, will be multiplied with the number of deltas for the total batch size.
+        params (list): List of the cosmological parameters with respect to which the perturbations to be used in
             training are returned, see the config for all possibilities. Defaults to None, then all are included
             according to the config.
         conf (str, dict, optional): Can be either a string (a config.yaml is read in), a dictionary (the config is
@@ -128,10 +131,13 @@ def get_fiducial_dset(
         i_noise (int): Index for the shape noise realizations. This has to be fixed and can't be a tf.Variable or
             other tensor (like randomly sampled).
         noise_scale (float): Factor by which to multiply the shape noise. This could also be a tf.Variable to change
-            it according to a schedule during training
+            it according to a schedule during training. Set to None to not include any shape noise. Defaults to 1.0.
+        is_cached (bool): Whether to cache on the level on the deserialized tensors. This is only feasible if all of
+            the fiducial .tfrecords fit into RAM. Defaults to False.
         n_readers (int, optional): Number of parallel readers, i.e. samples read out from different input files
             concurrently. This should be roughly less than a tenth of the number of files. Defaults to 8.
-        n_prefetch (int, optional): Number of dataset elements to prefetch.
+        n_prefetch (int, optional): Number of dataset elements to prefetch. Defaults to None, then tf.data.AUTOTUNE
+            is used.
         file_name_shuffle_buffer (int, optional): Defaults to 128.
         examples_shuffle_buffer (int, optional): Defaults to 128.
         file_name_shuffle_seed (int, optional): Defaults to 17.
@@ -140,7 +146,8 @@ def get_fiducial_dset(
             through it deterministically exactly once. Defaults to False.
         eval_seed (int, optional): Fixed seed for evaluation. Defaults to 32.
         input_context (tf.distribute.InputContext, optional): For distributed training, this is passed to the
-            dataset_fn like in https://www.tensorflow.org/tutorials/distribute/input#tfdistributestrategydistribute_datasets_from_function
+            dataset_fn like in
+            https://www.tensorflow.org/tutorials/distribute/input#tfdistributestrategydistribute_datasets_from_function
             Then, the dataset is sharded. Defaults to None for a non distributed dataset.
 
             Example usage:
@@ -154,7 +161,7 @@ def get_fiducial_dset(
 
     Returns:
         tf.data.Dataset: A dataset that returns samples with a given batchsize in the right ordering for the delta loss
-            The label consists of (i_example, i_noise)
+            The index label consists of (i_example, i_noise)
     """
     LOGGER.info(f"Starting to generate the fiducial training set for i_noise = {i_noise}")
 
@@ -164,6 +171,7 @@ def get_fiducial_dset(
     masks = tf.constant(analysis.get_tomo_masks(conf), dtype=tf.float32)
     n_z_bins = masks.shape[1]
     pert_labels = parameters.get_fiducial_perturbation_labels(params)
+    m_bias_dist = shear.get_m_bias_distribution(conf)
 
     if is_eval:
         tf.random.set_seed(eval_seed)
@@ -179,8 +187,9 @@ def get_fiducial_dset(
         dset = dset.shard(input_context.num_input_pipelines, input_context.input_pipeline_id)
         LOGGER.info(f"Sharding the dataset according to the input_context")
 
-    # repeat and shuffle
-    if not is_eval:
+    # repeat and shuffle the files
+    if not is_eval and not is_cached:
+        LOGGER.info(f"Shuffling file names")
         dset = dset.repeat()
         dset = dset.shuffle(file_name_shuffle_buffer, seed=file_name_shuffle_seed)
 
@@ -202,26 +211,35 @@ def get_fiducial_dset(
     # output signature (data_vectors, (i_example, i_noise))
     dset = dset.map(dset_parse_inverse, num_parallel_calls=tf.data.AUTOTUNE)
 
-    # add shear bias
-    m_bias_dist = shear.get_m_bias_distribution(conf)
-    dset = dset.map(lambda data_vectors, index: dset_add_bias(data_vectors, index, pert_labels, m_bias_dist))
+    if is_cached:
+        LOGGER.warning(f"Caching the dataset")
+        dset = dset.cache()
+        dset = dset.repeat()
 
-    # add noise
-    dset = dset.map(lambda data_vectors, index: dset_add_noise(data_vectors, index, pert_labels, noise_scale))
-
-    # shuffle and batch
-    if not is_eval:
+    # shuffle the tensors
+    if not is_eval and examples_shuffle_buffer is not None:
         dset = dset.shuffle(examples_shuffle_buffer, seed=examples_shuffle_seed)
+
+    # batch (first, for vectorization)
     dset = dset.batch(local_batch_size, drop_remainder=True)
     LOGGER.info(f"Batching into {local_batch_size} elements locally")
 
-    # concatenate the perturbations into the batch dimension like in
-    # https://github.com/des-science/y3-deep-lss/blob/main/deep_lss/utils/delta_loss.py#L167
-    dset = dset.map(lambda data_vectors, index: dset_concat_perts(data_vectors, index, pert_labels))
+    # augmentations (all in one function, to make parallelization easier)
+    dset = dset.map(
+        lambda data_vectors, index: dset_augmentations(
+            data_vectors,
+            index,
+            pert_labels,
+            m_bias_dist,
+            noise_scale,
+            masks,
+        ),
+        num_parallel_calls=tf.data.AUTOTUNE,
+    )
 
-    # mask to ensure the padding (relevant if there's additive shift somewhere). the masks tensor is broadcast
-    dset = dset.map(lambda data_vectors, index: (tf.multiply(data_vectors, masks), index))
-
+    # prefetch
+    if n_prefetch is None:
+        n_prefetch = tf.data.AUTOTUNE
     dset = dset.prefetch(n_prefetch)
 
     LOGGER.info(
@@ -241,8 +259,9 @@ def get_fiducial_multi_noise_dset(
     n_noise: int = 1,
     noise_scale: float = 1.0,
     # performance
+    is_cached: bool = False,
     n_readers: int = 8,
-    n_prefetch: int = tf.data.AUTOTUNE,
+    n_prefetch: int = None,
     file_name_shuffle_buffer: int = 128,
     examples_shuffle_buffer: int = 128,
     # random seeds
@@ -254,26 +273,42 @@ def get_fiducial_multi_noise_dset(
     """A dataset made up of the above datasets, but for different i_noise (index of the shape noise realization). The
     sampling is uniform.
 
-    Args:
         tfr_pattern (str): Glob pattern of the .fiducial tfrecord files.
-        batch_size (int): Local batch size, will be multiplied with the number of deltas for the total batch size.
-        params (list): List of the cosmological parameters with respect to which the perturbations to be used in 
+        local_batch_size (int): Local batch size, will be multiplied with the number of deltas for the total batch size.
+        params (list): List of the cosmological parameters with respect to which the perturbations to be used in
             training are returned, see the config for all possibilities. Defaults to None, then all are included
-            according to the config.        
+            according to the config.
         conf (str, dict, optional): Can be either a string (a config.yaml is read in), a dictionary (the config is
             passed through) or None (the default config is loaded). Defaults to None.
-        n_batches (int, optional): dset.take(n_batches) elements are taken from the dataset. This is done within this
-            function because this operation can not be performed on a distributed dataset.
         n_noise (int): Number of noise indices to include.
         noise_scale (float): Factor by which to multiply the shape noise. This could also be a tf.Variable to change
-            it according to a schedule during training
+            it according to a schedule during training. Set to None to not include any shape noise. Defaults to 1.0.
+        is_cached (bool): Whether to cache on the level on the deserialized tensors. This is only feasible if all of
+            the fiducial .tfrecords fit into RAM. Defaults to False.
         n_readers (int, optional): Number of parallel readers, i.e. samples read out from different input files
-            concurrently. Defaults to 8.
-        n_prefetch (int, optional): Number of dataset elements to prefetch.
+            concurrently. This should be roughly less than a tenth of the number of files. Defaults to 8.
+        n_prefetch (int, optional): Number of dataset elements to prefetch. Defaults to None, then tf.data.AUTOTUNE
+            is used.
         file_name_shuffle_buffer (int, optional): Defaults to 128.
         examples_shuffle_buffer (int, optional): Defaults to 128.
         file_name_shuffle_seed (int, optional): Defaults to 17.
         examples_shuffle_seed (int, optional): Defaults to 67.
+        is_eval (bool, optional): If this is True, then the dataset won't be shuffled repeatedly, such that one can go
+            through it deterministically exactly once. Defaults to False.
+        eval_seed (int, optional): Fixed seed for evaluation. Defaults to 32.
+        input_context (tf.distribute.InputContext, optional): For distributed training, this is passed to the
+            dataset_fn like in
+            https://www.tensorflow.org/tutorials/distribute/input#tfdistributestrategydistribute_datasets_from_function
+            Then, the dataset is sharded. Defaults to None for a non distributed dataset.
+
+            Example usage:
+                def dataset_fn(input_context):
+                    dset = fiducial_pipeline.get_fiducial_dset(
+                        tfr_pattern,
+                        params,
+                        batch_size,
+                        input_context=input_context,
+                    )
 
     Returns:
         tf.data.Dataset: A dataset that returns samples with a given batchsize in the right ordering for the delta loss
@@ -283,19 +318,20 @@ def get_fiducial_multi_noise_dset(
     dset = tf.data.Dataset.sample_from_datasets(
         [
             get_fiducial_dset(
-                tfr_pattern,
-                local_batch_size,
-                params,
-                conf,
-                i_noise,
-                noise_scale,
-                n_readers,
-                n_prefetch,
-                file_name_shuffle_buffer // n_noise,
-                examples_shuffle_buffer // n_noise,
-                file_name_shuffle_seed + i_noise,
-                examples_shuffle_seed + i_noise,
-                input_context,
+                tfr_pattern=tfr_pattern,
+                local_batch_size=local_batch_size,
+                params=params,
+                conf=conf,
+                i_noise=i_noise,
+                noise_scale=noise_scale,
+                is_cached=is_cached,
+                n_readers=n_readers,
+                n_prefetch=0,
+                file_name_shuffle_buffer=file_name_shuffle_buffer // n_noise,
+                examples_shuffle_buffer=examples_shuffle_buffer // n_noise,
+                file_name_shuffle_seed=file_name_shuffle_seed + i_noise,
+                examples_shuffle_seed=examples_shuffle_seed + i_noise,
+                input_context=input_context,
             )
             for i_noise in range(n_noise)
         ]
