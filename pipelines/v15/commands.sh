053171a8a7846de155e1b57bd23804fc0528d892
Arne Thomsen <athomsen@student.ethz.ch>
2025-09-16 07:57:13 -0700

    update debug notebooks

--- pipelines/v15/commands.sh
+++ pipelines/v15/commands.sh
@@ -10,3 +10,25 @@ esub ../../msfm/apps/run_grid_postprocessing.py \
     --job_name=tfr_grid_v15_debug --log_dir=/pscratch/sd/a/athomsen/run_files/v15/esub_logs \
     --system=slurm --source_file=../../pipelines/v15/perlmutter_setup.sh \
     --additional_slurm_args="--account=des,--constraint=cpu,--qos=shared,--licenses=cfs,--licenses=scratch"
+
+esub ../../msfm/apps/run_fiducial_postprocessing.py \
+    --n_files=1000 --no_derivatives \
+    --config=../../configs/v15/simple.yaml \
+    --dir_in=/global/cfs/cdirs/des/cosmogrid/processed/v11desy3/CosmoGrid/bary \
+    --dir_out=/pscratch/sd/a/athomsen/v11desy3/v15/extended/tfrecords/fiducial \
+    --cosmogrid_version="1.1" \
+    --mode=jobarray --function=rerun_missing --n_jobs=1000 --max_njobs=1000 --tasks="0>1000" --keep_submit_files \
+    --job_name=tfr_fidu_v15 --log_dir=/pscratch/sd/a/athomsen/run_files/v15/esub_logs \
+    --system=slurm --source_file=../../pipelines/v15/perlmutter_setup.sh \
+    --additional_slurm_args="--account=des,--constraint=cpu,--qos=shared,--licenses=cfs,--licenses=scratch"
+
+esub ../../msfm/apps/run_fiducial_postprocessing.py \
+    --n_files=1000 --no_derivatives \
+    --config=../../configs/v15/simple.yaml \
+    --dir_in=/global/cfs/cdirs/des/cosmogrid/processed/v11desy3/CosmoGrid/bary \
+    --dir_out=/pscratch/sd/a/athomsen/v11desy3/v15/extended/tfrecords/fiducial \
+    --cosmogrid_version="1.1" \
+    --mode=jobarray --function=merge --n_jobs=1000 --max_njobs=1000 --tasks="0>1000" --keep_submit_files \
+    --job_name=tfr_fidu_v15 --log_dir=/pscratch/sd/a/athomsen/run_files/v15/esub_logs \
+    --system=slurm --source_file=../../pipelines/v15/perlmutter_setup.sh \
+    --additional_slurm_args="--account=des,--constraint=cpu,--qos=shared,--licenses=cfs,--licenses=scratch"
